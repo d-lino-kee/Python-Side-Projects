a55cca9416311394110f3cebf21f63fbd1dc0c6d
Lino Kee <144301761+d-lino-kee@users.noreply.github.com>
2025-05-02 06:01:29 +0000

    PIG project work in progress

--- password_manager.py
+++ password_manager.py
@@ -1,18 +1,41 @@
+from cryptography.fernet import Fernet
 
-master_pwd = input("What is the master password? ")
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+'''
+
+def load_key():
+    file =  open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+# master_pwd = input("What is the master password? ")
+key = load_key() #+ master_pwd.encode()
+fer = Fernet(key)
+
+# key + password + text to encrypt = random text
+# random text + key + passwrod = text to encrypt
 
 def view():
     with open('passwords.txt', 'r') as f:
         for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split("|")
-            print("User:", user, "Password:", passw)
+            print("User:", user, "Password:", 
+                  fer.decrypt(passw.encode()).decode())
+
+
 def add():
     name = input('Account Name: ')
     pwd = input("Password: ")
 
     with open('passwords.txt', 'a') as f:
-        f.write(name + "|" + pwd + "\n")
+        f.write(name + "|" + fer.encyrpt(pwd.encode()).decode() + "\n")
         
 
 while True:
